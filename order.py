91919dd7ffa8cebf6c81906d29e7ab3e18f4b305
supreet <65367030+Supreet984@users.noreply.github.com>
2022-01-29 14:34:28 +0530

    Update order.py

--- order.py
+++ order.py
@@ -3,22 +3,37 @@ import math
 import requests 
 import pandas as pd
 import numpy as np
+from tkcalendar import Calendar
+
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+
+mycol = mydb["stocks"]
+
+mycol.drop()
+
+mycol = mydb["stocks"]
+
 
 root = Tk()
-root.title(" DBS Order Book System")
+root.title("Order Book System")
 root.configure(background='black')
 
 label1 = Label(root, text="Order book System", fg="white", font="Helvetica 30 bold italic underline",bg='black').place(x=500, y=0)
 
-button1 = Button(root, text="Customer Screen",width=20,height='2',fg="white",font="times 13 bold",bg='red', command=lambda:Customer()).place(x=1400,y=50)
+button1 = Button(root, text="Customer Screen",width=20,height='2',fg="white",font="times 13 bold",bg='red', command=lambda:Customer(flag)).place(x=1000,y=50)
 
-
-flag = 1
+global flag
+flag  = 1
 
 def OpenM():
+	global flag 
 	flag = 1
 
-def Closem():
+def CloseM():
+	global flag 
 	flag = 0
 
 label = Label(root,text="Admin Screen",fg='red',bg='yellow', font = '30')
@@ -28,7 +43,7 @@ label.place(x=40,y=50)
 button1 = Button(root, text="Open Market",width=11,fg="white",font="Arial 13 bold",bg='red',command=lambda:OpenM())
 button1.place(x=150, y=130)
 
-button1 = Button(root, text="Close Market",width=11,fg="white",font="Arial 13 bold",bg='red',command=lambda:OpenM())
+button1 = Button(root, text="Close Market",width=11,fg="white",font="Arial 13 bold",bg='red',command=lambda:CloseM())
 button1.place(x=330, y=130)
 
 l1 = Label(root,text="Select a Stock",fg='white',bg='red', font = '30')
@@ -74,7 +89,11 @@ l1.place(x=750,y=490)
 
 #-------------------------------// Customer Screen //----------------------------------------------
 
-def Customer():
+global StockName, price, OrderType, Quantity
+
+
+
+def Customer(flag):
     nwin=Toplevel()
     nwin.configure(background='black')
 
@@ -85,26 +104,22 @@ def Customer():
         label = Label(nwin,text="Market's closed",fg='red',bg='yellow', font = '30')
         label.place(x=40,y=60)
     else:
-
         l1 = Label(nwin,text="Search a Stock",fg='white',bg='red', font = '30')
         l1.place(x=40,y=60)
 
         options = ['DBS', 'AAPL', 'GOOGL', 'FB', 'BARC']
-        df = StringVar(nwin)
+        df = StringVar(root)
         df.set("Select a Stock")
-        drop = OptionMenu( nwin ,df, *options )
+        drop = OptionMenu(nwin ,df, *options )
         drop.config(bg='antique white')
         drop.place(x='40',y='100')
+        StockName = df.get()
+        print(StockName)
 
         l2 = Label(nwin,text="Order Type",fg='white',bg='red', font = '30')
         l2.place(x=40,y=160)
 
-        o1 = ['Limit', 'Market']
-        d1 = StringVar(nwin)
-        d1.set("Select Market Type")
-        drop = OptionMenu( nwin ,d1, *o1 )
-        drop.config(bg='antique white')
-        drop.place(x='40',y='190')
+
 
         l2 = Label(nwin, text="Price (if market order)", bg='red',fg="white", font="times 15")
         l2.place(x=40, y=240)
@@ -116,20 +131,22 @@ def Customer():
         v2= Entry(nwin, width =20,borderwidth=2, bg='antique white')
         v2.place(x=40,y=350)
 
+        Quantity = str(v2.get())
+
         button1 = Button(nwin, text="Place Order",width=22,fg="white",font="times 15 bold",bg='red', command=lambda:Order()).place(x=40,y=400)
-
-
     nwin.geometry('500x600')
-
-
-#---------------------------------------------------------------------------------------------------
-
-
-
 
 #------------------------------- // Backend //----------------------------------------------#
 
+#---------------------------------------------------------------------------------------------------
 
+def Order():
+    StockName = df.get()
+    print(StockName)
+    Price = str(v.get())
+    print(Price)
+    Quantity = str(v2.get())
+    print(Quantity)
 
-root.state('zoomed')
+root.geometry('1400x600')
 root.mainloop()
